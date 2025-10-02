7a307d7a2099c0cbb444fb90d55e7a291007372e
Heesu Cho <101083889@georgebrown.ca>
2025-10-02 16:20:13 -0400

    verification screen ui design

--- app/(auth)/VerificationScreen.tsx
+++ app/(auth)/VerificationScreen.tsx
@@ -1,7 +1,11 @@
+import Screen from '@/components/Screen';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import React, { useState } from 'react';
-import { Alert, Button, Keyboard, SafeAreaView, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
+import { Alert, SafeAreaView, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
 import apiClient from '../../api';
+import GradientBackground from '../../components/GradientBackground';
+import Button from "../../components/ui/Button";
+
 
 const VerificationScreen = () => {
   const [code, setCode] = useState('');
@@ -28,25 +32,63 @@ const VerificationScreen = () => {
   };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Enter Verification Code</Text>
-        <Text style={styles.text}>A 6-digit code was sent to {email}.</Text>
-        <TextInput style={styles.input} placeholder="6-digit code" value={code} onChangeText={setCode} keyboardType="number-pad" maxLength={6} textAlign="center"/>
-        <View style={styles.buttonContainer}>
-          <Button title="Verify" onPress={handleVerify} />
-        </View>
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
+    <GradientBackground>
+      <Screen>
+            <TouchableWithoutFeedback>
+              <SafeAreaView style={styles.container}>
+                <Text style={styles.title}>EmoBudget</Text>
+                <Text className='font-flex text-base text-white text-center my-10 tracking-widest'> My smart budget tracker </Text>
+                <View className='w-0 h-20 border-r border-r-white mb-10'></View>
+                
+                <Text className='font-flexBold text-3xl text-bold text-white text-center tracking-widest'> Verification </Text>
+                <Text className='font-flex text-base text-white text-center mt-10 mb-4 tracking-widest'>Enter Verification Code</Text>
+                <Text style={styles.text}>A 6-digit code will send to {email}.</Text>
+                <TextInput className='focus:outline-none' style={styles.input} placeholder="6-digit code" value={code} onChangeText={setCode} keyboardType="number-pad" maxLength={6} textAlign="center"/>
+                <View style={styles.buttonContainer}>
+                  <Button label="Verify" onPress={handleVerify} />
+                </View>
+              </SafeAreaView>
+            </TouchableWithoutFeedback>
+      </Screen>
+    </GradientBackground>
   );
 };
 
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20, backgroundColor: '#f5f5f5' },
-  title: { fontSize: 28, fontWeight: 'bold', color: '#333', marginBottom: 40 },
-  input: { width: '100%', height: 50, backgroundColor: '#fff', borderWidth: 1, borderColor: '#ddd', borderRadius: 8, paddingHorizontal: 15, marginBottom: 15, fontSize: 18 },
-  text: { fontSize: 16, color: '#666', marginBottom: 20, textAlign: 'center' },
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    padding: 20, 
+  },
+  title: { 
+    fontSize: 40,          
+    fontWeight: "bold",
+    color: "#fff", 
+    textAlign: "center",
+    marginBottom: 0,
+    textShadowColor: "rgba(255, 255, 255, 0.9)", 
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 12,
+    marginTop:120   
+  },
+  input: { 
+    width: 350, 
+    height: 50, 
+    borderBottomWidth: 1,
+    borderBottomColor: "#fff",
+    color: "#fff",              
+    paddingVertical: 8,
+    marginBottom: 20,
+    fontSize:20,
+  },
+  text: { 
+    fontSize: 16, 
+    color: '#fafafa', 
+    marginBottom: 20, 
+    textAlign: 'center' 
+  },
   buttonContainer: { width: '100%', marginTop: 10 },
 });
 
